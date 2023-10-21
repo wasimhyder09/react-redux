dc12e8117288ade3f63345dc7df404afd180e51a
Wasim Hyder <wasim@creativefaze.com>
2023-10-21 20:21:08 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Provider><App /></Provider>);
