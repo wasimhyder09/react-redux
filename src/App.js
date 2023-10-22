371514d2f0905b06d3c69d78834fdde489e6585d
Wasim Hyder <wasim@creativefaze.com>
2023-10-22 13:42:42 +0500

    Apply multiple state slices for authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux';
+
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
+  const auth = useSelector(state => state.auth.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
+      {auth && <UserProfile />}
+      {!auth && <Auth />}
       <Counter />
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
 import classes from './Header.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from '../store/index';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth.isAuthenticated);
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {auth &&
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>My Products</a>
+            </li>
+            <li>
+              <a href='/'>My Sales</a>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </nav>}
     </header>
   );
 };
