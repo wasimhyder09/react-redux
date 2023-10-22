9c849acc58596988cccd46b8801ecd04f08d40d9
Wasim Hyder <wasim@creativefaze.com>
2023-10-22 13:15:25 +0500

    Added new auth slice for working with multiple slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit'
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuthenticated: false };
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: { counter: 0, showCounter: true },
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -19,8 +22,22 @@ const counterSlice = createSlice({
   }
 });
 
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }
 });
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
